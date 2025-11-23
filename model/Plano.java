007ac4525c48972e6cfdf04c67617bd155829e1a
juanvictor1 <juannn8755h@gmail.com>
2025-11-23 15:04:34 -0300

    save

--- model/Plano.java
+++ model/Plano.java
@@ -1,14 +1,33 @@
 package model;
 
 public class Plano {
-    private String tipo;
+    private String nome;
     private double preco;
 
-    public Plano(String tipo, double preco) {
-        this.tipo = tipo;
+    public Plano(String nome, double preco) {
+        this.nome = nome;
         this.preco = preco;
     }
 
-    public String getTipo() { return tipo; }
-    public double getPreco() { return preco; }
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public static final Plano MENSAL = new Plano("Mensal", 90.0);
+    public static final Plano SEMESTRAL = new Plano("Semestral", 480.0);
+    public static final Plano ANUAL = new Plano("Anual", 900.0);
+
+    public void exibirPlano() {
+        System.out.println("Plano: " + nome);
+        System.out.println("Preço: R$ " + preco);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - R$ " + preco;
+    }
 }
